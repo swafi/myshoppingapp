8a657e8718e0ec330079dd52d833118593b11ecf
swafi <swafikom@gmail.com>
2024-07-20 11:28:55 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,353 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Mazao_Connect());
+}
+
+class Mazao_Connect extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mazao Connect',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false, // Remove debug banner
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Mazao Connect',
+          style: TextStyle(
+            fontWeight: FontWeight.bold, // Bold text
+          ),
+        ),
+        backgroundColor: Colors.green[200], // Background color
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: <Widget>[
+          // Background image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/bac.jpeg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => BrowseProductsScreen()),
+                    );
+                  },
+                  child: Text(
+                    'Browse Products',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold, // Bold text
+                    ),
+                  ),
+                ),
+                SizedBox(height: 75),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RegisterScreen()),
+                    );
+                  },
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold, // Bold text
+                    ),
+                  ),
+                ),
+                SizedBox(height: 75),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold, // Bold text
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BrowseProductsScreen extends StatelessWidget {
+  final Map<String, List<String>> products = {
+    'Chicken': [
+      'Broiler Chicken',
+      'Boneless Broiler',
+      'Kienyeji Chicken',
+      'Ex-Broiler'
+    ],
+    'Fish': [
+      'Tilapia',
+      'Catfish',
+      'Fish Fillet'
+    ],
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Browse Products',
+          style: TextStyle(
+            fontWeight: FontWeight.bold, // Bold text
+          ), 
+        ),
+        backgroundColor: Colors.green, // Background color
+      ),
+      body: Stack(
+        children: <Widget>[
+          // Background image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/url.jpeg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Content
+          ListView(
+            children: products.keys.map((category) {
+              return ExpansionTile(
+                title: Text(
+                  category,
+                  style: TextStyle(
+                    color: Colors.black, // Changed text color to black
+                    fontSize: 20.0, // Increased font size to 20.0
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                children: products[category]!.map((item) {
+                  return ListTile(
+                    title: Text(
+                      item,
+                      style: TextStyle(
+                        color: Colors.black, // Changed text color to black
+                        fontSize: 25.0, // Font size for sub-items
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Register',
+          style: TextStyle(
+            fontWeight: FontWeight.bold, // Bold text
+          ),
+        ),
+        backgroundColor: Colors.green, // Background color
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Registering...')),
+                    );
+                  }
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold, // Bold text
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Login',
+          style: TextStyle(
+            fontWeight: FontWeight.bold, // Bold text
+          ),
+        ),
+        backgroundColor: Colors.green, // Background color
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Logging in...')),
+                    );
+                  }
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold, // Bold text
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
